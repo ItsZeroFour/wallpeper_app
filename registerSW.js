0f96afa2ef309f7c84cfd72182e2d61e19074bcc
itsZeroFour <itsZeroFourX@gmail.com>
2025-07-13 18:13:56 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/wallpaper_app/sw.js', { scope: '/wallpaper_app/' })})}
